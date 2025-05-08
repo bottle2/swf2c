9eda2eb4850e8af8e0b89789ee05718903199b4e
bottle2 <bento.schirmer@gmail.com>
2025-05-08 07:19:03 -0300

    Some test, some buttons

--- main.c
+++ main.c
@@ -43,8 +43,8 @@ EM_JS(void, init_canvas, (), {
 # define SHOW \
     there_she_is_render_html5(get_canvas(), frame_i);
 #elif defined(FEAT_PLUTOVG) 
-SDL_Renderer *renderer;
-SDL_Texture *texture;
+static SDL_Renderer *renderer;
+static SDL_Texture *texture;
 # define INIT there_she_is_init_plutovg(); \
     TRY(!(renderer = SDL_CreateRenderer(window, -1, 0))); \
     TRY(!(texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, there_she_is_width, there_she_is_height)))
@@ -73,34 +73,63 @@ static uint64_t start;
 
 static void iter(void)
 {
+    #define EVAL_FRAME (frame_0 + (SDL_GetTicks64() - start) / framerate)
+
+    static bool is_play = true;
+    static int frame_0 = 0;
+
     for (SDL_Event event; SDL_PollEvent(&event); )
     {
         if (SDL_QUIT == event.type)
         {
-	    CLEAR;
+	    END;
             SDL_DestroyWindow(window);
             SDL_QuitSubSystem(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
             LOOP_END;
         }
-	else if (SDL_KEYDOWN == event.type) switch (event.key.keysym.sym)
+	else if (SDL_KEYDOWN == event.type)
         {
-            case 'p':
-            break;
+            if (event.key.keysym.sym >= '0' && event.key.keysym.sym <= '9')
+            {
+                frame_0 = there_she_is_n_frame / 10 * (event.key.keysym.sym - '0');
+                start = SDL_GetTicks64();
+            }
+            else switch (event.key.keysym.sym)
+            {
+                case ' ': // Fall through.
+                case 'p':
+                    if ((is_play = !is_play))
+                        start = SDL_GetTicks64();
+                    else
+                        frame_0 = EVAL_FRAME;
+                break;
+                case '.':
+                    if (!is_play && frame_0 < there_she_is_n_frame - 1)
+                        frame_0++;
+                break;
+                case ',':
+                    if (!is_play && frame_0 > 0)
+                        frame_0--;
+                break;
+            }
         }
     }
 
     CLEAR;
 
-    int frame_i = (SDL_GetTicks64() - start) / framerate;
+    int frame_i = is_play ? EVAL_FRAME : frame_0;
     if (frame_i < 0)
         frame_i = 0;
     if (frame_i >= there_she_is_n_frame)
     {
         start = SDL_GetTicks64();
         frame_i = 0;
+        frame_0 = 0;
     }
 
     SHOW;
+
+    #undef EVAL_FRAME
 }
 
 int main(int argc, char *argv[])
